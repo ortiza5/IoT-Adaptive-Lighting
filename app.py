22f8a566d3a59c3a3132cb8d0ab86cb24175e1f7
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2019-04-12 06:58:55 -0400

    added MQTT broker support
    Mosquitto MQTT Broker on Linux now needs to be configured for this code to run

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template, flash, redirect
+from flask_mqtt import Mqtt
+import ssl
+import subprocess
+import atexit
+import time
 from config import *
 
-
+# verify with 'netstat -at'
+subprocess.Popen('sudo mosquitto -c /etc/mosquitto/mosquitto.conf', shell=True)
+time.sleep(5)
 app = Flask(__name__)
+app.config['MQTT_BROKER_URL'] = '0.0.0.0'
+app.config['MQTT_BROKER_PORT'] = 8883
+app.config['MQTT_USERNAME'] = ''
+app.config['MQTT_PASSWORD'] = ''
+app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+# TLS Settings
+app.config['MQTT_TLS_ENABLED'] = True
+app.config['MQTT_TLS_INSECURE'] = True
+app.config['MQTT_TLS_VERSION'] = ssl.PROTOCOL_TLSv1_2
+app.config['MQTT_TLS_CA_CERTS'] = '/etc/mosquitto/ca_certificates/ca.crt'
+mqtt = Mqtt(app)
 app.secret_key = SECRET
 
 
@@ -12,5 +30,26 @@ def startpage():
     return render_template('startpage.html')
 
 
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    print(data)
+
+
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    print(level, buf)
+
+def exit_handler():
+    # subprocess.Popen('sudo mosquitto -c /etc/mosquitto/mosquitto.conf', shell=True)
+    print('My application is ending!')
+
+
 if __name__ == '__main__':
+    atexit.register(exit_handler)
+    # sudo mosquitto -c /etc/mosquitto/mosquitto.conf
+    # subprocess.Popen('sudo mosquitto -c /etc/mosquitto/mosquitto.conf', shell=True)
     app.run(host="0.0.0.0", debug=True)
