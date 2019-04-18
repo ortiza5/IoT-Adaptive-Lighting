4a830e6691f34bd6caca668cd38f761c045a20d5
Default <a@a.a>
2019-04-18 13:32:55 -0400

    Better commenting and global variables

--- app.py
+++ app.py
@@ -6,15 +6,37 @@ import atexit
 import time
 from config import *
 
+# Activate python enviroment:
+# source env/bin/activate
+
+# Deactivate python enviroment:
+# deactivate
+# =========================================================================
+# Global Variables:
+intensity = 1
+r_val = 255
+b_val = 255
+g_val = 255
+num_leds = 300
+length = 5	#(in meters)
+leds_per_m = num_leds/length
+
+# =========================================================================
+# Start the MQTT broker in another thread:
+# Service stopped in the exithandler function (CTRL-C)
 # verify with 'netstat -at'
-subprocess.Popen('sudo mosquitto -c /etc/mosquitto/mosquitto.conf', shell=True)
+subprocess.Popen('sudo mosquitto -c mosquitto.conf', shell=True)
+# Sleep to allow password to be entered (TODO - find a better way)
 time.sleep(5)
+
+# =========================================================================
+# Start the Flask App and Configure MQTT 
 app = Flask(__name__)
 app.config['MQTT_BROKER_URL'] = '0.0.0.0'
 app.config['MQTT_BROKER_PORT'] = 8883
 app.config['MQTT_USERNAME'] = ''
 app.config['MQTT_PASSWORD'] = ''
-app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+app.config['MQTT_REFRESH_TIME'] = 0.5  # refresh time in seconds
 # TLS Settings
 app.config['MQTT_TLS_ENABLED'] = True
 app.config['MQTT_TLS_INSECURE'] = True
@@ -23,33 +45,53 @@ app.config['MQTT_TLS_CA_CERTS'] = '/etc/mosquitto/ca_certificates/ca.crt'
 mqtt = Mqtt(app)
 app.secret_key = SECRET
 
-
+# =========================================================================
+# ****************************HTML Pages***********************************
+# =========================================================================
 # Startpage
 @app.route('/')
 def startpage():
-    return render_template('startpage.html')
+	return render_template('startpage.html')
 
+@app.route('/activity_log')
+def activity_log():
+	return render_template('activity_log.html')
 
+@app.route('/mapping')
+def mapping():
+	return render_template('mapping.html')
+
+@app.route('/settings', methods=['GET', 'POST'])
+def settings():
+	return render_template('settings.html')
+
+# =========================================================================
+# **************************MQTT Functions*********************************
+# =========================================================================
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
-    data = dict(
-        topic=message.topic,
-        payload=message.payload.decode()
-    )
-    print(data)
+	data = dict(
+		topic=message.topic,
+		payload=message.payload.decode()
+	)
+	print(data)
 
 
 @mqtt.on_log()
 def handle_logging(client, userdata, level, buf):
-    print(level, buf)
+	print(level, buf)
 
+# =========================================================================
+# ***************************Helper Functions******************************
+# =========================================================================
+# Called when CTRL-C is pressed to cleanly stop the MQTT broker
 def exit_handler():
-    # subprocess.Popen('sudo mosquitto -c /etc/mosquitto/mosquitto.conf', shell=True)
-    print('My application is ending!')
+	print('Stopping Mosquitto')
+	subprocess.Popen('sudo service mosquitto stop', shell=True)
+	print('The application is closing')
 
-
+# =========================================================================
+# Start up the app
 if __name__ == '__main__':
-    atexit.register(exit_handler)
-    # sudo mosquitto -c /etc/mosquitto/mosquitto.conf
-    # subprocess.Popen('sudo mosquitto -c /etc/mosquitto/mosquitto.conf', shell=True)
-    app.run(host="0.0.0.0", debug=True)
+	atexit.register(exit_handler)
+	app.run(host="0.0.0.0", debug=True)
