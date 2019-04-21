f15909f99681f8f5215ff1c1df48c6d113181189
Default <a@a.a>
2019-04-21 16:00:09 -0400

    more python environment file changes (still maybe need gitignore?)

---
+++ static/js/socket_script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+	var socket = io.connect('http://0.0.0.0:5000');
+
+	socket.on('connect', function() {
+		socket.send('User has connected!');
+	});
+
+	socket.on('mqtt_rec', function(data) {
+		$("#log_data").append('<p> Received on: ['+data['topic']+'] Data: '+data['payload']+'</p>');
+		console.log(data['payload'])
+	});
+
+	socket.on('mqtt_pub', function(data) {
+		$("#log_data").append('<p> Published: X: '+data['x']+'  Y: '+data['y']+'</p>');
+		console.log(data['payload'])
+	});
+});
